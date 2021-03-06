6bf0e0fc32f47b1e6ecaa3912f37c524508b9495
Abayomi Akinfemiwa <Abayomi.Akinfemiwa@stanbicibtc.com>
2021-03-06 10:22:40 +0100

    create chron_IS_ALIVE project

---
+++ src/main/java/com/possible/isalive/IsAliveApplication.java
@@ -0,0 +1,13 @@
+package com.possible.isalive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsAliveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsAliveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/possible/isalive/IsAliveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.possible.isalive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsAliveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
